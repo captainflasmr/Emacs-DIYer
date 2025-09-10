2659c2ceffca490e4b36a8f64f96694147ceb63f
james dyer <captainflasmr@gmail.com>
2025-09-10 07:12:59 +0100

    General tidying

--- init.el
+++ init.el
@@ -121,11 +121,6 @@ Each line is represented as a list of field values."
       (push (cons (nth i header) (nth i line)) result))
     (reverse result)))
 
-(defun my-icomplete-exit-minibuffer-with-input ()
-  "Exit the minibuffer with the current input, without forcing completion."
-  (interactive)
-  (exit-minibuffer))
-
 (defun my/quick-window-jump ()
   "Jump to a window by typing its assigned character label.
 If there is only a single window, split it horizontally.
